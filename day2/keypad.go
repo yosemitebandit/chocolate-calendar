1c69edd7718d778ed47ee9cd4e11b86de68bbc36
Matt Ball <matt.ball.2@gmail.com>
2016-12-15 13:48:17 -0800

    adds day2 part 2

--- day2/keypad.go
+++ day2/keypad.go
@@ -11,16 +11,6 @@ type position struct {
 	y float64
 }
 
-func clamp(value float64) float64 {
-	if value > 1 {
-		return 1
-	}
-	if value < -1 {
-		return -1
-	}
-	return value
-}
-
 func main() {
 	data, _ := ioutil.ReadFile("input.txt")
 
@@ -31,27 +21,50 @@ func main() {
 		'L': {x: -1, y: 0},
 	}
 
-	positionConverter := map[position]int{
-		{x: -1, y: 1}:  1,
-		{x: 0, y: 1}:   2,
-		{x: 1, y: 1}:   3,
-		{x: -1, y: 0}:  4,
-		{x: 0, y: 0}:   5,
-		{x: 1, y: 0}:   6,
-		{x: -1, y: -1}: 7,
-		{x: 0, y: -1}:  8,
-		{x: 1, y: -1}:  9,
+	allowedMoves := map[position]string{
+		{x: 0, y: 2}:   "D",
+		{x: -1, y: 1}:  "RD",
+		{x: 0, y: 1}:   "URDL",
+		{x: 1, y: 1}:   "LD",
+		{x: -2, y: 0}:  "R",
+		{x: -1, y: 0}:  "URDL",
+		{x: 0, y: 0}:   "URDL",
+		{x: 1, y: 0}:   "URDL",
+		{x: 2, y: 0}:   "L",
+		{x: -1, y: -1}: "UR",
+		{x: 0, y: -1}:  "URDL",
+		{x: 1, y: -1}:  "UL",
+		{x: 0, y: -2}:  "U",
 	}
 
-	currentPosition := position{x: 0, y: 0}
-	var doorCode []int
+	positionConverter := map[position]rune{
+		{x: 0, y: 2}:   '1',
+		{x: -1, y: 1}:  '2',
+		{x: 0, y: 1}:   '3',
+		{x: 1, y: 1}:   '4',
+		{x: -2, y: 0}:  '5',
+		{x: -1, y: 0}:  '6',
+		{x: 0, y: 0}:   '7',
+		{x: 1, y: 0}:   '8',
+		{x: 2, y: 0}:   '9',
+		{x: -1, y: -1}: 'A',
+		{x: 0, y: -1}:  'B',
+		{x: 1, y: -1}:  'C',
+		{x: 0, y: -2}:  'D',
+	}
+
+	currentPosition := position{x: -2, y: 0}
+	var doorCode []string
 
 	for _, element := range strings.Fields(string(data)) {
 		for _, value := range element {
-			currentPosition.x = clamp(currentPosition.x + runeConverter[value].x)
-			currentPosition.y = clamp(currentPosition.y + runeConverter[value].y)
+			// Check if the move is allowed based on the current position.
+			if strings.Contains(allowedMoves[currentPosition], string(value)) {
+				currentPosition.x += runeConverter[value].x
+				currentPosition.y += runeConverter[value].y
+			}
 		}
-		doorCode = append(doorCode, positionConverter[currentPosition])
+		doorCode = append(doorCode, string(positionConverter[currentPosition]))
 	}
 
 	fmt.Println("Part 1 Solution:", doorCode)
