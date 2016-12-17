97e5c487c8424dc6734b41c5ec2b46edb144affd
Matt Ball <matt.ball.2@gmail.com>
2016-12-17 09:21:59 -0800

    adds 5.2

--- day5/door.go
+++ day5/door.go
@@ -5,11 +5,17 @@ import (
 	"encoding/hex"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+type char struct {
+	value byte
+}
 
 func main() {
 	input := "uqwqemis"
-	var password []byte
+	letters := "abcdef"
+	var password [8]char
 	i := 0
 	for {
 		data := fmt.Sprintf("%s%s", input, strconv.Itoa(i))
@@ -17,14 +23,57 @@ func main() {
 		hasher.Write([]byte(data))
 		hash := hex.EncodeToString(hasher.Sum(nil))
 		if hash[0:5] == "00000" {
-			fmt.Println(i, "->", hash)
-			password = append(password, hash[5])
-			if len(password) == 8 {
+
+			// Print what we got.
+			fmt.Println(hash)
+			index := string(hash[5])
+
+			// If the index is a letter, skip.
+			if strings.Contains(letters, index) {
+				fmt.Println("letter!")
+				fmt.Println()
+				i += 1
+				continue
+			}
+
+			// If the index is greater than the pw length, skip.
+			integerIndex, _ := strconv.Atoi(index)
+			if integerIndex > len(password)-1 {
+				fmt.Println("index too large!")
+				fmt.Println()
+				i += 1
+				continue
+			}
+
+			// If we've already filled that index, skip.
+			if !((char{}) == password[integerIndex]) {
+				fmt.Println("already have that index!")
+				fmt.Println()
+				i += 1
+				continue
+			}
+
+			var c char
+			c.value = hash[6]
+			password[integerIndex] = c
+
+			//Check if array has nil.
+			nilPresent := false
+			for _, value := range password {
+				if (char{}) == value {
+					nilPresent = true
+				}
+			}
+			if !nilPresent {
 				break
 			}
+			fmt.Println()
 		}
 		i += 1
 	}
 
-	fmt.Println("Part 1 Solution:", string(password))
+	fmt.Println("Part 2 Solution:")
+	for _, c := range password {
+		fmt.Print(string(c.value))
+	}
 }
