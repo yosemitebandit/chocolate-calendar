ecdaa1c4d83ca36aaa79271d0cd87a4a3ea4b1a3
Matt Ball <matt.ball.2@gmail.com>
2016-12-18 09:40:53 -0800

    adds 6.1

---
+++ day6/repeat.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func contains(values []int, target int) bool {
+	for _, element := range values {
+		if element == target {
+			return true
+		}
+	}
+	return false
+}
+
+type letterCount struct {
+	letter byte
+	count  int
+}
+
+func main() {
+	var code []string
+	for col := 0; col < 8; col += 1 {
+		letterFrequency := map[byte]int{}
+		data, _ := ioutil.ReadFile("input.txt")
+
+		for _, row := range strings.Split(string(data), "\n") {
+			if row == "" {
+				continue
+			}
+			_, ok := letterFrequency[row[col]]
+			if ok {
+				letterFrequency[row[col]] += 1
+			} else {
+				letterFrequency[row[col]] = 1
+			}
+		}
+
+		var mostFrequentLetter letterCount
+		mostFrequentLetter.letter = 'a'
+		mostFrequentLetter.count = 0
+		for letter, count := range letterFrequency {
+			if count > mostFrequentLetter.count {
+				mostFrequentLetter.letter = letter
+				mostFrequentLetter.count = count
+			}
+		}
+		code = append(code, string(mostFrequentLetter.letter))
+	}
+	fmt.Println("Part 1 Solution:", strings.Join(code, ""))
+}
