ffbe43ff0089bd322e158b31cd6e924cd396a990
Matt Ball <matt.ball.2@gmail.com>
2016-12-20 23:09:35 -0500

    adds rotation parsing tests

--- day8/display.go
+++ day8/display.go
@@ -17,6 +17,13 @@ func parseRectCommand(command string) []int {
 	return result
 }
 
+func parseRotateCommand(command string) []int {
+	elements := strings.Fields(command)
+	field, _ := strconv.Atoi(strings.Split(elements[2], "=")[1])
+	amount, _ := strconv.Atoi(elements[4])
+	return []int{field, amount}
+}
+
 func main() {
 	// Init the display.
 	const displayRows int = 6
@@ -30,7 +37,7 @@ func main() {
 
 	// Work through the instructions.
 	data, _ := ioutil.ReadFile("input.txt")
-	for command_index, command := range strings.Split(string(data), "\n") {
+	for _, command := range strings.Split(string(data), "\n") {
 		if command == "" {
 			continue
 		}
@@ -41,17 +48,33 @@ func main() {
 					display[row][col] = "#"
 				}
 			}
-			// Plot it.
-			var output []string
-			for i := 0; i < displayRows; i++ {
-				output = append(output, strings.Join(display[i][:], ""))
-				output = append(output, "\n")
+		} else if strings.Contains(command, "rotate row") {
+			fmt.Println("row!")
+		} else if strings.Contains(command, "rotate column") {
+			fmt.Println("col!")
+		}
+
+		// Plot it.
+		var output []string
+		for i := 0; i < displayRows; i++ {
+			output = append(output, strings.Join(display[i][:], ""))
+			output = append(output, "\n")
+		}
+		fmt.Printf("\033[0;0H")
+		fmt.Println(strings.Join(output, ""))
+		time.Sleep(time.Second / 4)
+
+	}
+
+	// Count the 'on' pixels.
+	activatedPixels := 0
+	for row := 0; row < displayRows; row++ {
+		for col := 0; col < displayCols; col++ {
+			if display[row][col] == "#" {
+				activatedPixels++
 			}
-			fmt.Printf("\033[0;0H")
-			fmt.Println(strings.Join(output, ""))
-			fmt.Println(command_index, dims)
-			time.Sleep(time.Second)
 		}
 	}
+	fmt.Println("Activated Pixels:", activatedPixels)
 
 }
--- day8/display_test.go
+++ day8/display_test.go
@@ -5,12 +5,12 @@ import (
 	"testing"
 )
 
-type rectParseTestInstance struct {
+type testInstance struct {
 	input  string
 	output []int
 }
 
-var rectParseCommandTests = []rectParseTestInstance{
+var rectParseCommandTests = []testInstance{
 	{"rect 3x2", []int{3, 2}},
 	{"rect 1x1", []int{1, 1}},
 	{"rect 10x9", []int{10, 9}},
@@ -20,6 +20,21 @@ func TestRectCommandParsing(t *testing.T) {
 	for _, test := range rectParseCommandTests {
 		if !reflect.DeepEqual(parseRectCommand(test.input), test.output) {
 			t.Error("Fail, input:", test.input, "Got:", parseRectCommand(test.input))
+		}
+	}
+}
+
+var rotateTests = []testInstance{
+	{"rotate column x=46 by 2", []int{46, 2}},
+	{"rotate column x=1 by 3", []int{1, 3}},
+	{"rotate row y=12 by 1", []int{12, 1}},
+	{"rotate column x=18 by 17", []int{18, 17}},
+}
+
+func TestRotateCommand(t *testing.T) {
+	for _, test := range rotateTests {
+		if !reflect.DeepEqual(parseRotateCommand(test.input), test.output) {
+			t.Error("Fail, input:", test.input, "Got:", parseRotateCommand(test.input))
 		}
 	}
 }
